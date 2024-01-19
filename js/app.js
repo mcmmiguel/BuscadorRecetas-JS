71daff1edf7a3e2622118fede3f1bb08b1b20842
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-18 19:53:13 -0600

    Mostrar categorias de la API

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 function iniciarApp() {
+
+    const selectCategorias = document.querySelector('#categorias');
+
     obtenerCategorias();
 
     function obtenerCategorias() {
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
         fetch(url)
             .then(respuesta => respuesta.json())
-            .then(datos => console.log(datos.categories))
-    }
-}
+            .then(datos => mostrarCategorias(datos.categories));
+
+    };
+
+    function mostrarCategorias(categorias = []) {
+        categorias.forEach(({ strCategory }) => {
+            const option = document.createElement('OPTION');
+            option.value = strCategory;
+            option.textContent = strCategory;
+            selectCategorias.appendChild(option);
+        });
+    };
+};
 
 document.addEventListener('DOMContentLoaded', iniciarApp);
